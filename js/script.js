99ef9f3a8640eb1827681d6fe54405fa74f004be
Dylan McGee <46721669+dtmcgee-su@users.noreply.github.com>
2021-06-28 21:53:50 -0400

    Cleaned up code and added comments

--- js/script.js
+++ js/script.js
@@ -3,21 +3,7 @@ Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
 
-
-
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
-
-
-
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
-*/
-
+//Display 9 students with all their info from database
 function showPage(list, page) {
    let start = (page * 9) - 9;
    let end = (page * 9);
@@ -28,7 +14,7 @@ function showPage(list, page) {
          let studentItem = `<li class="student-item cf">
             <div class="student-details">
                <img class="avatar" src=${data[i].picture.thumbnail} alt="Profile Picture">
-               <h3>${data[i].name.first} ${data[i].name.last}</h3>
+               <h3 class = "name">${data[i].name.first} ${data[i].name.last}</h3>
                <span class = "email">${data[i].email}</span>
             </div>
             <div class="joined-details">
@@ -38,10 +24,10 @@ function showPage(list, page) {
          `;
          ul.insertAdjacentHTML('beforeend', studentItem);
       }
-
    }
 }
 
+// create buttons which will show a dfifferent set of students depending on the page clicked
 function addPagination(list){
    const maxPages = Math.ceil(data.length / 9);
    const ulLinkList = document.querySelector('.link-list');
@@ -53,10 +39,8 @@ function addPagination(list){
       </li>`;
       ulLinkList.insertAdjacentHTML('beforeend', button);
    }
-
     const nextPageButton = document.querySelector('button');
     nextPageButton.className = 'active';
-
    ulLinkList.addEventListener('click', (e) => {
       if (e.target.tagName === 'BUTTON'){
          const activeButton = document.querySelector('.active');
@@ -66,6 +50,7 @@ function addPagination(list){
       }
    })
 }
+
 // Call functions
 showPage(data, 1);
 addPagination(data);
